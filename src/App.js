09dbe544bb9d116981b15c725b23221cccca5a70
liam roberts <liamroberts77@googlemail.com>
2020-08-27 20:52:34 +0100

    used weather description from api call instead of weather main

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ function App() {
             <div className="temp">
               {Math.round(weather.main.temp)}°c
             </div>
-            <div className="weather">{weather.weather[0].main}</div>
+            <div className="weather">{weather.weather[0].description}</div>
           </div>
           </div>
           ) : (  <div>
